ff9fce3b068686d7a29c43e660f142a363987020
Rafael Monteiro Porto <rafael.monteiro.porto@outlook.com>
2020-10-26 01:46:49 -0300

    Corrigido problema no campo Input number de decimal.

--- src/FlyCalculator.Web/Models/CalculoAutonomia.cs
+++ src/FlyCalculator.Web/Models/CalculoAutonomia.cs
@@ -1,4 +1,6 @@
-﻿namespace FlyCalculator.Web.Models
+﻿using System;
+
+namespace FlyCalculator.Web.Models
 {
     public class CalculoAutonomia
     {
@@ -8,5 +10,23 @@
         public Autonomia Adicional { get; set; } = new Autonomia();
         public Autonomia MinimoRequirido { get; set; } = new Autonomia();
         public Autonomia Total { get; set; } = new Autonomia();
+
+        public void CalculaMinimoRequirido()
+        { 
+            MinimoRequirido.Tempo = Etapa.Tempo + Alternado.Tempo + Reserva.Tempo;
+            MinimoRequirido.Litros = CalculaQuantidade(MinimoRequirido.Tempo);
+        }
+
+        public void CalculaTotal()
+        { 
+            Total.Tempo = Etapa.Tempo + Alternado.Tempo + Reserva.Tempo + Adicional.Tempo;
+            Total.Litros = CalculaQuantidade(Total.Tempo);
+        }
+
+        public decimal CalculaQuantidade(TimeSpan tempo)
+        {
+            var cessna = 22.0M; //consumo de litro por hora
+            return cessna * (decimal)tempo.TotalHours;
+        }
     }
 }
